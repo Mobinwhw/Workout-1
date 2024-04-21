6430f33e79a55dde1819cb792c14f0242ef8e354
Mobinwhw <166295006+Mobinwhw@users.noreply.github.com>
2024-04-21 11:54:21 -0700

    Add files via upload

---
+++ haeri_2.ino
@@ -0,0 +1,26 @@
+int ledpinred = 5;
+int ledpinyellow = 3;
+ int ledpingreen = 4;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(ledpinred,OUTPUT);
+ pinMode(ledpinyellow,OUTPUT);
+ pinMode(ledpingreen,OUTPUT);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(ledpingreen,HIGH);
+digitalWrite(ledpinyellow,LOW);
+digitalWrite(ledpinred,LOW);
+delay(3000);
+digitalWrite(ledpinyellow,HIGH);
+digitalWrite(ledpingreen,LOW);
+digitalWrite(ledpinred,LOW);
+delay(1000);
+digitalWrite(ledpinred,HIGH);
+digitalWrite(ledpinyellow,LOW);
+digitalWrite(ledpingreen,LOW);
+delay(2000);
+}
